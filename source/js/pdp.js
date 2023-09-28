5cba108f7fd3cdd13eeb013e574c657e196829ff
Dymedrol <dmitrykob@gmail.com>
2023-09-28 17:37:16 +0700

    update

--- source/js/pdp.js
+++ source/js/pdp.js
@@ -10,18 +10,13 @@ export const initPdp = () => {
 
     const selects = form.find('.js-pdp-select');
     const selectedOptions = {};
-    let selectedOptionsCount;
+    const selectedOptionsCount = Object.keys(selectedOptions).length;
 
-    selects.each(function() {
-        selectedOptions[this.name] = this.value;
-    });
-
-    selectedOptionsCount = Object.keys(selectedOptions).length;
-
-    selects.on('change paste keyup', function() {
-        const selectedValue = this.value;
-        selectedOptions[this.name] = selectedValue;
-
+    const updateMainInput = function(value, name) {
+        console.log('change', value, name)
+        const selectedValue = value;
+        selectedOptions[name] = selectedValue;
+        console.log(selectedOptions)
         mainOptions.each(function() {
             const text = $(this).text();
             const optionId = $(this).val();
@@ -35,9 +30,65 @@ export const initPdp = () => {
 
             if (coincidenceCount === selectedOptionsCount) {
                 mainSelect.val(optionId);
+                console.log('val 1', mainSelect.find('option:selected').val())
             }
-
         });
+    }
+
+    selects.each(function() {
+        selectedOptions[this.name] = this.value;
+    });
+
+    // selects.on('change paste keyup', function() {
+    //     alert('change')
+    //     const selectedValue = this.value;
+    //     selectedOptions[this.name] = selectedValue;
+
+    //     mainOptions.each(function() {
+    //         const text = $(this).text();
+    //         const optionId = $(this).val();
+    //         let coincidenceCount = 0;
+
+    //         Object.keys(selectedOptions).forEach(key => {
+    //           if (text.includes(selectedOptions[key])) {
+    //             coincidenceCount++;
+    //           }
+    //         });
+
+    //         if (coincidenceCount === selectedOptionsCount) {
+    //             mainSelect.val(optionId);
+    //         }
+
+    //     });
+    // });
+
+    console.log(selectedOptions)
+
+    $('.rosa-pdp-info-controls-item').click(function() {
+        const wrapper = $(this).closest('.rosa-pdp-select-wrapper');
+        wrapper.find('.rosa-pdp-info-controls-item-menu').toggle();
+    });
+
+    $('.rosa-pdp-info-controls-item-menu').click(function(event) {
+        const target = $(event.target);
+        const wrapper = target.closest('.rosa-pdp-select-wrapper');
+        const selectedName = wrapper.find('select').attr("name");
+
+        if (target.hasClass("rosa-pdp-info-controls-item-menu-el")) {
+            // if (target.hasClass('laoli-pdp-mobile-info-buttons-sizes-menu-item_empty')) {
+            //     status.text(madeToOrderText);
+            //     addButton.val(addToCartText);
+            // } else {
+            //     status.text(inStockText);
+            //     addButton.val(addToCartText);
+            // }
+            const selectedValue = $(event.target).text();
+
+            wrapper.find('.rosa-pdp-info-controls-item span').text(selectedValue);
+            wrapper.find('select').val($(event.target).attr('data-value'));
+            updateMainInput(selectedValue, selectedName);
+            $(this).hide();
+        }
     });
 }
 
