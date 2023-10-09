4543d42d79822fc6c598f120f91f7ecb59bc2b24
Dymedrol <dmitrykob@gmail.com>
2023-10-09 21:23:57 +0700

    update

--- source/js/collection-slider-with-side-pic.js
+++ source/js/collection-slider-with-side-pic.js
@@ -1,11 +1,8 @@
 export const initCollectionSliderWithSidePic = () => {
     $(".rosa-collection-slider2-info-slider").owlCarousel({
         margin: 0,
-        smartSpeed: 500,
         center:true,
         items: 1,
-        animateIn: 'ease-out',
-        animateOut: 'ease-out',
         loop: false,
         dots: true,
     });
