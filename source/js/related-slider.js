40d9f0e6a9196af1eaca963fbe2528e359d7ac0f
Dymedrol <dmitrykob@gmail.com>
2023-10-23 18:30:38 +0700

    update

--- source/js/related-slider.js
+++ source/js/related-slider.js
@@ -1,25 +1,25 @@
 export const initRelatedSlider = () => {
 
-    // setTimeout(() => {
-    //   $(".rosa-related-slider-inner").owlCarousel({
-    //       margin: 0,
-    //       autoWidth: true,
-    //       smartSpeed: 500,
-    //       animateIn: 'ease-out',
-    //       animateOut: 'ease-out',
-    //       loop: true,
-    //       dots: false,
-    //   });
+    setTimeout(() => {
+      $(".rosa-related-slider-custom-inner").owlCarousel({
+          margin: 16,
+          autoWidth: true,
+          smartSpeed: 500,
+          animateIn: 'ease-out',
+          animateOut: 'ease-out',
+          loop: true,
+          dots: false,
+      });
 
-    //   var owl = $(".rosa-related-slider-inner");
-    //   owl.owlCarousel();
-    //   $(".rosa-related-slider-header-nav-arrow-next").click(function () {
-    //     owl.trigger("next.owl.carousel");
-    //   });
-    //   $(".rosa-related-slider-header-nav-arrow-prev").click(function () {
-    //     owl.trigger("prev.owl.carousel");
-    //   });
-    // }, "2000");
+      var owl = $(".rosa-related-slider-custom-inner");
+      owl.owlCarousel();
+      $(".rosa-related-slider-header-nav-arrow-next").click(function () {
+        owl.trigger("next.owl.carousel");
+      });
+      $(".rosa-related-slider-header-nav-arrow-prev").click(function () {
+        owl.trigger("prev.owl.carousel");
+      });
+    }, "2000");
 }
 
 
