b5ca3fce86ef3eea0af88133c63ca58347c91a4b
Dymedrol <dmitrykob@gmail.com>
2023-11-17 17:27:40 +0700

    jumbotron autoplay added

--- source/js/jumbo-slider.js
+++ source/js/jumbo-slider.js
@@ -1,9 +1,13 @@
 export const initJumboSlider = () => {
+
     $(".rosa-jumbotron-slider").owlCarousel({
         margin: 0,
         items: 1,
         autoHeight: false,
         dots: true,
+        autoplay: jumbotronSettings.autoplay,
+        autoplayTimeout: jumbotronSettings.autoplayTimeout * 1000,
+        loop: jumbotronSettings.loop,
     });
 }
 
